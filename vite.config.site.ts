ae8f22f8d929567ea2ac5603a67f7ea2bc44e689
wxs <wu-xuesong@qq.com>
2023-03-31 16:21:23 +0800

    feat: add github page

--- vite.config.site.ts
+++ vite.config.site.ts
@@ -3,9 +3,6 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import { resolve } from "node:path";
-
-const resolveFile = (path: string = "") => resolve(__dirname, "src/lib", path);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,24 +10,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  build: {
-    lib: {
-      entry: {
-        main: resolveFile("index"),
-        "compilers/template": resolveFile("compilers/template"),
-        "plugins/api": resolveFile("plugins/api"),
-      },
-      name: "VueTopCom",
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
-        },
-      },
     },
   },
 });
